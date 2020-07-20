33fdb288ecd96f22a9cbc41a81adb75be4b21c4b
Rahul Chandra <1rahulchandra1@gmail.com>
2020-07-20 16:42:02 +0530

    First Commit using Bash Timestamp(2020-07-20-16:41:59)

---
+++ lib/main.dart
@@ -0,0 +1,124 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'pokemon.dart';
+import 'pokemondetail.dart';
+
+void main() => runApp(
+      MaterialApp(
+        title: 'Pokemon App',
+        home: HomePage(),
+        debugShowCheckedModeBanner: false,
+        theme: _lightTheme,
+      ),
+    );
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  PokeHub pokeHub;
+  var url =
+      "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    var res = await http.get(url);
+    // print(res.body);
+    var decodeJson = jsonDecode(res.body);
+    pokeHub = PokeHub.fromJson(decodeJson);
+    print(pokeHub.toJson());
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Pokemon App'),
+      ),
+      body: pokeHub == null
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : GridView.count(
+              crossAxisCount: 3,
+              children: pokeHub.pokemon
+                  .map(
+                    (poke) => Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => PokeDetail(
+                                pokemon: poke,
+                              ),
+                            ),
+                          );
+                        },
+                        child: Hero(
+                          tag: poke.img,
+                          child: Card(
+                            elevation: 3.0,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Container(
+                                  height: 100,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: NetworkImage(poke.img),
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  poke.name,
+                                  style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
+    );
+  }
+}
+
+ThemeData _lightTheme = ThemeData(
+  primaryColor: Color(0xFFe46d47),
+  accentColor: Color(0xFFe46d47),
+  textTheme: TextTheme(
+    bodyText1: TextStyle(
+      fontSize: 18,
+      color: Color(0xFF3d4152),
+      fontWeight: FontWeight.w600,
+    ),
+    bodyText2: TextStyle(
+      fontSize: 12,
+      color: Color(0xFFa9abb2),
+    ),
+    button: TextStyle(
+      fontSize: 14,
+      color: Colors.white,
+    ),
+  ),
+);
